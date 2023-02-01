1d43117a44753b340cb86595c0f67ffa3e4fe2e1
Marcelo Pantoja <marcelo.pantoja@mpap.mp.br>
2023-01-31 21:25:27 -0300

    estilo de tabela nas listagens clientes e produtos

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -1,8 +1,23 @@
 <x-layout title="Clientes">
     <a class="btn btn-dark mb-2" href="/clientes/criar">Adicionar Cliente</a>
-    <ul class="list-group">
-        @foreach($clientes as $cliente)
-        <li class="list-group-item">{{ $cliente['nome'] }}</li>
-        @endforeach 
-    </ul>
+    <table class="table table-striped table-bordered">
+        <thead>
+            <tr>
+              <th>id</th>
+              <th>Nome do Cliente</th>
+              <th>CPF</th>
+              <th>Telefone</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($clientes as $cliente)
+              <tr>
+                <td>{{ $cliente['id'] }}</td>
+                <td>{{ $cliente['nome'] }}</td>
+                <td>{{ $cliente['cpf'] }}</td>
+                <td>{{ $cliente['telefone'] }}</td>
+            </tr>
+            @endforeach 
+          </tbody>
+    </table>
 </x-layout>
--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -1,8 +1,24 @@
 <x-layout title="Produtos">
     <a class="btn btn-dark mb-2" href="/produtos/criar">Adicionar Produto</a>
-    <ul class="list-group">
-        @foreach($produtos as $produto)
-        <li class="list-group-item">{{ $produto['nome'] }}</li>
-        @endforeach 
-    </ul>
+    <table class="table table-striped table-bordered">
+        <thead>
+            <tr>
+              <th>id</th>
+              <th>Produto</th>
+              <th>Preço Unitário</th>
+              <th>Estoque</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($produtos as $produto)
+              <tr>
+                <td>{{ $produto['id'] }}</td>
+                <td>{{ $produto['nome'] }}</td>
+                <td>{{ $produto['precounitario'] }}</td>
+                <td>{{ $produto['quantidade'] }}</td>
+            </tr>
+            @endforeach 
+          </tbody>
+    </table>
+    
 </x-layout>
